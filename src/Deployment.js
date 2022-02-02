e3c9c869b32dffe4015cb4d7463bbea9a17c4c18
Elizabeth Johnstone <ewj218@nyu.edu>
2022-02-01 20:15:40 -0500

    Attempting Heroku deployment

--- src/Deployment.js
+++ src/Deployment.js
@@ -2,5 +2,5 @@
 Assign baseURL to window.location.origin for Heroku deployment. Assign it to `http://localhost:4000` to run locally.
 */
 
-export const baseURL = window.location.origin
+export const baseURL = `window.location.origin`
 // export const baseURL = `http://localhost:4000`;
